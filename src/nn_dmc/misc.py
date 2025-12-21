ab5f7df08d10320d97618e036e4b33eb1267e094
gretaja <gretaja@uw.edu>
2025-12-20 18:46:45 -0800

    move make_training_set function to misc module

--- src/nn_dmc/misc.py
+++ src/nn_dmc/misc.py
@@ -12,6 +12,39 @@ import pyvibdmc as pv
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
+def make_training_set(ml_path, system, isomers, sim_types, sim_nums):
+    """
+    Function that takes in specified MOB-ML simulations for the isomers of OH-W5
+    to form a training data set for NN training
+
+    example input:
+    ml_path = 'C:/Users/gjaco/OneDrive/Documents/UW/McCoy Group/Machine Learning'
+    system = 'h11o6'
+    isomers = ['c5']
+    sim_types = ['min', 'wide', 'reg']
+    sim_nums = [[1], [1], [1,2,3,4,5]]
+
+    Returns coordinates array and energies array 
+    """
+    path = f'{ml_path}/{system}/training_data'
+
+    total_cds = []
+    total_energies = []
+
+    for isom in isomers:
+        for i, sim in enumerate(sim_types):
+            for num in sim_nums[i]:
+                cds = np.load(f'{path}/{system}_{isom}_mobml_{sim}_sim_{num}_training_cds.npy')
+                energies = np.load(f'{path}/{system}_{isom}_mobml_{sim}_sim_{num}_training_energies.npy')
+
+                total_cds.append(cds)
+                total_energies.append(energies)
+
+    total_cds = np.concatenate(total_cds)
+    total_energies = np.concatenate(total_energies)
+
+    return total_cds, total_energies
+
 def read_xyz_file(filename):
     """
     Reads in a .xyz file (in Bohr) and returns an nx3 numpy array of the coordinates in Angstroms
